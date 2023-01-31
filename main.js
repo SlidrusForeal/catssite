d05a0dcb08c0ff77ca3366997cb613c04bb17c86
K1zikk <49024907+K1zikk@users.noreply.github.com>
2023-01-31 18:25:07 +0300

    safsa

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const newsButton = document.getElementById('getNews');
 
 //Store the URL of the JSON we want to retrieve in a variable
 const requestURL = 'https://newsapi.org/v2/top-headlines?' + //top 20 headlines 
-          'country=us&category=business&' + //filters
+          'country=ru&category=business&' + //filters
           'apiKey=93b1fdd888374b6cac2a407c8efdfee6'; //my own API key from newsapi.org
 
 // ASYNC/AWAIT
